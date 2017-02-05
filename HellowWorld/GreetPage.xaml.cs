ba25f54e53f411f484e577e34ffc0fbfed5dcde3
Mahmoud Hboubati <MHboubati@Mahmouds-MacBook-Pro.local>
2017-02-05 21:22:04 +0400

    first commit

---
+++ HellowWorld/GreetPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HellowWorld
+{
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+		{
+			label.Text = String.Format("Value is {0:F2}", e.NewValue);
+		}
+		
+	}
+}
